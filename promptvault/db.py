419154a5a600c4db41671e3f6e12d841699338f8
Sohail <mohdsohailahmed46@gmail.com>
2026-08-02 04:42:04 -0400

    feat: implement crud.py

--- promptvault/db.py
+++ promptvault/db.py
@@ -13,7 +13,9 @@ class Database:
     def get_connection(self) -> sqlite3.Connection:
         self._ensure_data_folder_exists()
 
-        connection: sqlite3.Connection = sqlite3.connect(self.DB_PATH)
+        connection: sqlite3.Connection = sqlite3.connect(
+            self.DB_PATH, detect_types=sqlite3.PARSE_DECLTYPES
+        )
         connection.row_factory = sqlite3.Row
 
         connection.execute("PRAGMA foreign_keys = ON")
